05fddbe350b3d7990a1a33befdc7b0505b206034
Alexandru Liviu Nita <alexandru.nita@microsoft.com>
2021-02-04 14:11:50 +0200

    initial project setup
    initial project setup

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+import sqlite3
+
+from helpers import login_required, apology
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# root route function
+@app.route("/")
+@login_required
+def index():
+    """ Show current Team availability """
+    return apology("TODO", 200)
+
+@app.route("/login")
+def login():
+    """ Login form """
+    return apology("TODO", 200)
